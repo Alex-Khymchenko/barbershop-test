f7f59efa5773108b2638da0da838c2eaa752c24d
Alex-Khymchenko <bbiorage1@gmail.com>
2023-05-24 21:47:17 +0300

    Up to date

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const isUserLoggedIn = false;
+
+//If else operator
+
+// if (isUserLoggedIn) {
+//     alert("You are logged in!");
+// } else {
+//     alert("You are not logged in!")
+// }
+
+//Ternary operator
+
+// isUserLoggedIn ? alert("You are logged in!") : alert("You are not logged in!");
+
+//Switch operator
+
+// const age = 30;
+// switch(age) {
+//     case 10: 
+//     alert("Happy birthday, give me a kiss!");
+//     break;
+//     case 20: 
+//     alert("Happy birthday, give me a hug!");
+//     break;
+//     case 30: 
+//     alert("Happy birthday, give me a grandchildren!");
+//     break;
+//     default:
+//         alert("Happy birthday, give me your legacy!")
+// }
